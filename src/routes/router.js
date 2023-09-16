a27c790dacc398471284153c5b6c595f2b5cbea4
Ravi Rathore <rathoreravi1508@gmail.com>
2023-09-16 16:47:26 +0530

    update paths

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Contact from "../pages/Contact";
-const path = process.env.PUBLIC_URL;
 
 const router = createBrowserRouter(
   [
